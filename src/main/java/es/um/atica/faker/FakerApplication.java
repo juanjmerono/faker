20977a96b0fb7d5076c7605cc7d6be04d5742822
juanjmerono <jjmerono@um.es>
2023-03-15 17:31:21 +0100

    Add initial project.

---
+++ src/main/java/es/um/atica/faker/FakerApplication.java
@@ -0,0 +1,13 @@
+package es.um.atica.faker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/um/atica/faker/FakerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.um.atica.faker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
